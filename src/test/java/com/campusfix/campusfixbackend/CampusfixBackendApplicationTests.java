431c8a16eb2bc63a9e62faa215fa757b3a6d6f21
Prikshit <prikshitkuhar@gmail.com>
2026-02-13 22:22:51 +0530

    initial project setup

---
+++ src/test/java/com/campusfix/campusfixbackend/CampusfixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusfix.campusfixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusfixBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
